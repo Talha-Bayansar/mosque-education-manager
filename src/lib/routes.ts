528e62689a4a23f38414e0eef7a9944e3349268f
Talha Bayansar <talha.bayansar@hotmail.com>
2024-07-21 00:50:23 +0200

    adding routes for newly added default pages

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,5 +3,26 @@ export const routes = {
   signin: { root: "/sign-in" },
   dashboard: {
     root: "/dashboard",
+    groups: {
+      root: "/dashboard/groups",
+    },
+    meetups: {
+      root: "/dashboard/meetups",
+    },
+    people: {
+      root: "/dashboard/people",
+    },
+    posters: {
+      root: "/dashboard/posters",
+    },
+    settings: {
+      root: "/dashboard/settings",
+    },
+    tasks: {
+      root: "/dashboard/tasks",
+    },
+    team: {
+      root: "/dashboard/team",
+    },
   },
 };
