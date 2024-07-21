59cbf57c9c3f4ec271dd1969cdfc86fb9a8c141d
Talha Bayansar <talha.bayansar@hotmail.com>
2024-07-21 22:44:25 +0200

    refactored app-table component

--- src/features/person/components/people-table.tsx
+++ src/features/person/components/people-table.tsx
@@ -2,81 +2,17 @@
 
 import type { Person } from "@prisma/client";
 import { usePeople } from "../hooks/use-people";
-import {
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/shared/components/ui/table";
-import { useTranslations } from "next-intl";
 import { usePeopleTableColumns } from "../hooks/use-people-table-columns";
+import { AppTable } from "@/shared/components/app-table";
 
 type Props = {
   peopleServer: Person[];
 };
 
 export const PeopleTable = ({ peopleServer }: Props) => {
-  const t = useTranslations();
   const columns = usePeopleTableColumns();
 
   const { data } = usePeople(peopleServer);
-  const table = useReactTable({
-    data: data!,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
 
-  return (
-    <div className="rounded-md border w-full">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
+  return <AppTable columns={columns} data={data!} />;
 };
