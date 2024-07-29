899ec7e6b909b7c76b3e04c8800f399d9ff1fdf6
Talha Bayansar <talha.bayansar@hotmail.com>
2024-07-29 03:21:17 +0200

    reverting changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 import { locales } from "./config";
 
-const intlMiddleware = createMiddleware({
+export default createMiddleware({
   // A list of all locales that are supported
   locales: locales,
 
@@ -10,19 +9,7 @@ const intlMiddleware = createMiddleware({
   defaultLocale: locales[0],
 });
 
-// Exclude /api/uploadthing from the internationalization middleware
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // Skip i18n middleware for /api/uploadthing
-  if (pathname.startsWith("/api/uploadthing")) {
-    return NextResponse.next();
-  }
-
-  return intlMiddleware(request);
-}
-
 export const config = {
-  // Match all paths except for /api/uploadthing
-  matcher: ["/(?!api/uploadthing).*"],
+  // Match only internationalized pathnames
+  matcher: ["/", `/(nl|tr)/:path*`],
 };
