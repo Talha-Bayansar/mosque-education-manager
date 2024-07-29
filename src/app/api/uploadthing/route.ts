5aa675b58b43901736b7340a62bf288627cc7f35
Talha Bayansar <talha.bayansar@hotmail.com>
2024-07-29 02:55:28 +0200

    adding cors package

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,11 @@
 import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
+import Cors from "micro-cors";
+
+Cors({
+  allowMethods: ["POST", "GET"],
+});
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
